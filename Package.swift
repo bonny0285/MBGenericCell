8ec930c1f044f66d93f99316a945512b5796aed4
Massimiliano <massimiliano.bonafede@altran.it>
2021-10-06 12:09:12 +0200

    added self dependencies

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MBGenericCell",
-            dependencies: []),
+            dependencies: ["MBGenericCell"]),
         .testTarget(
             name: "MBGenericCellTests",
             dependencies: ["MBGenericCell"]),
